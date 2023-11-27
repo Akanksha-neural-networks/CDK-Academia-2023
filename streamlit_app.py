6445a9c173b4610c9f218ac5ede56d94222a5f4b
Akanksha Prasad <101962662+Akanksha-neural-networks@users.noreply.github.com>
2023-11-27 10:18:39 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,11 @@ streamlit.dataframe(fruits_to_show)
 
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+'''fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 #take json format and normalise it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -47,4 +47,17 @@ streamlit.dataframe(my_data_row)
 fruit_choice1 = streamlit.text_input('What fruit would you like information about?','jackfruit')
 streamlit.write('Thanks for adding ', fruit_choice1)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")'''
+
+try:
+  fruit_choice = streamlit.text_input ('What fruit would you like information about?')
+  if not fruit choice:
+      streamlit.error("Please select a fruit to get information. ")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json ())
+      streamlit.dataframe(fruityvice_normalized)
+      
+except URLError as e:
+    streamlit.error ()
+
